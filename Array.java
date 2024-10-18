2eca8fe1ad9d839594158ed8d5d3036c31a1f0b7
sudeep vishwakarm <160476411+sudeepv44@users.noreply.github.com>
2024-10-18 12:26:36 +0530

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,22 @@
+
+package coreJava;
+
+public class Array {
+	public static void main(String args[]) {
+		int arr[][] = { {1,2,3,4,5,6},
+				{1,2,3,45 } ,{ 1,3,2,4,3} };
+	int a=10;
+	if(a<100) {
+		System.out.println("a");
+		
+		//break;
+		System.out.println("b");
+	}
+		//System.out.println(arr.hashCode());
+		
+		//for (int i = 0; i < arr.length; i++) {
+		//	System.out.println(arr.equals(arr));
+		//}
+	}
+
+}
